618e9910a334c564a59b89161749de234e36cd74
Jincan Lin <34576058+ihotarubi@users.noreply.github.com>
2020-09-19 10:57:55 +0800

    logistic回归

---
+++ logistic_gradAscent/logRegres.py
@@ -0,0 +1,60 @@
+from numpy import *
+
+
+# 加载数据集
+def loadDataSet():
+    dataMat = []
+    labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
+
+def sigmoid(inX):
+    return 1.0 / (1 + exp(-inX))
+
+
+# 梯度上升算法
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = mat(dataMatIn)  # convert to NumPy matrix
+    labelMat = mat(classLabels).transpose()  # convert to NumPy matrix
+    m, n = shape(dataMatrix)
+    alpha = 0.001  # 向目标移动的步长
+    maxCycles = 500  # 迭代次数
+    weights = ones((n, 1))
+    for k in range(maxCycles):  # heavy on matrix operations
+        h = sigmoid(dataMatrix * weights)  # matrix multi
+        error = (labelMat - h)  # vector subtraction
+        weights = weights + alpha * dataMatrix.transpose() * error  # matrix multi
+    return weights
+
+
+def plotBestFit(weights):
+    import matplotlib.pyplot as plt
+    dataMat, labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
